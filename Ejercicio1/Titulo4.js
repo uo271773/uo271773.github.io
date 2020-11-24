ee869c2056b96271c17c8890accf6f71dffe4ebd
uo271773 <60694785+uo271773@users.noreply.github.com>
2020-11-24 18:44:24 +0100

    Add files via upload

---
+++ Ejercicio1/Titulo4.js
@@ -0,0 +1,5 @@
+/* Titulo4.js */
+// Version 1.0 15/11/2020. Juan Rodríguez de la Fuente. Universidad de Oviedo
+document.write("<h4>");
+document.write(informacion.universidad);
+document.write("</h4>");
